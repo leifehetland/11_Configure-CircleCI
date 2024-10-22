daf3b77f5b0a9c8293a47f768c54bd3adb4d88e6
leifehetland <lehetland1@gmail.com>
2024-10-22 15:01:29 -0400

    modified header to trigger workflow

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -3,7 +3,7 @@ const Header = () => {
     <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <h1 className="m-0">Word Guess</h1>
-        <p className="m-0">Guess the movie title!</p>
+        <p className="m-0">Guess the title of the film title!</p>
       </div>
     </header>
   );
